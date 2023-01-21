793e0be35fcf87cef96505e9285853db83f346f7
taeksoolee <leets1490@gmail.com>
2023-01-21 22:59:41 +0900

    feat: navigation 작업중

---
+++ src/components/Navigator.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+  import nav from '../nav';
+</script>
+
+<nav class="navigation">
+  <ul>
+    <li><a href="/">Home</a></li>
+    <li>
+      <a href="/develop">Develop</a>
+      <ul>
+        <li>
+          <a href="/develop/react">React</a>
+          <ul>
+            <li><a href="/develop/react/getting-start">Getting Start</a></li>
+          </ul>
+        </li>
+        <li>
+          <a href="/develop/nextjs">NestJS</a>
+          <ul>
+            <li><a href="/develop/nextjs/getting-start">Getting Start</a></li>
+          </ul>
+        </li>
+        <li>
+          <a href="/develop/svelte">Svelte</a>
+          <ul>
+            <li><a href="/develop/svelte/getting-start">Getting Start</a></li>
+          </ul>
+        </li>
+      </ul>
+    </li>
+    <li>
+      <a href="/tech-review">Thch Review</a>
+      <ul>
+        <li>
+          <a href="/tech-review/anker-spacesound-q45">Space spacesound Q45</a>
+        </li>
+      </ul>
+    </li>
+  </ul>
+</nav>
+
+<style>
+  .navigation {
+
+  }
+</style>
---
+++ src/nav.ts
@@ -0,0 +1,52 @@
+export default [
+	{
+		title: 'Home',
+		link: '/'
+	},
+	{
+		title: 'Develop',
+		link: '/develop',
+		children: [
+			{
+				title: 'React',
+				link: '/develop/react',
+				children: [
+					{
+						title: 'Getting Start',
+						link: '/develop/react/getting-start'
+					}
+				]
+			},
+			{
+				title: 'NextJS',
+				link: '/develop/nextjs',
+				children: [
+					{
+						title: 'Getting Start',
+						link: '/develop/nextjs/getting-start'
+					}
+				]
+			},
+			{
+				title: 'Svelte',
+				link: '/develop/svelte',
+				children: [
+					{
+						title: 'Getting Start',
+						link: '/develop/svelte/getting-start'
+					}
+				]
+			}
+		]
+	},
+	{
+		title: 'Tech Review',
+		link: '/tech-review',
+		children: [
+			{
+				title: 'Space spacesound Q45',
+				link: '/tech-review/anker-space'
+			}
+		]
+	}
+];
