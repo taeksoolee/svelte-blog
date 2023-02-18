ab2505bd5a72dd4723e773821bbb01713512e4e1
taeksoolee <leets1490@gmail.com>
2023-02-18 23:26:44 +0900

    style: layout 수정

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,23 +1,29 @@
-<script lang="ts">
+<script context="module">
 	import '../app.scss';
+</script>
 
+<script lang="ts">
 	import SearchInput from 'src/components/SearchInput.svelte';
 	import Navigator from 'src/components/Navigator.svelte';
 	import RecentView from 'src/components/RecentView.svelte';
 </script>
 
 <div>
-	<section>
-		<Navigator />
-		<hr />
-		<RecentView />
-		<hr />
+	<section class="">
 		<SearchInput />
+		<hr />
+		<div class="flex mx-4">
+			<div class="w-60">
+				<Navigator />
+			</div>
+			<main class="h-full flex-1">
+				<slot />
+			</main>
+			<div class="w-60">
+				<RecentView />
+			</div>
+		</div>
 	</section>
-
-	<main>
-		<slot />
-	</main>
 </div>
 
 <style>
